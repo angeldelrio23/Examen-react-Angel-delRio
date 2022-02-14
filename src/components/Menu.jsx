c22a31af01940c1ed4fadd366b219ae1d4aa5b3b
angeldelrio23 <angeldelrio23@yahoo.es>
2022-02-14 12:32:57 +0100

    Ejercicio1Hecho

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -16,40 +16,34 @@ class Menu extends React.Component {
   constructor(props) {
     super(props);
   }
-
   render() {
     return (
-      <>
-        <Navbar bg="light" expand={false}>
-          <Container fluid>
-            <Navbar.Brand href="#">Repaso examen</Navbar.Brand>
-            <Navbar.Toggle aria-controls="offcanvasNavbar" />
-            <Navbar.Offcanvas
-              id="offcanvasNavbar"
-              aria-labelledby="offcanvasNavbarLabel"
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id="offcanvasNavbarLabel">
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  {PaginasApp.map((item) => {
-                    return (
-                      <Nav.Link as={Link} to={item.path}>
-                        {item.title}
-                      </Nav.Link>
-                    );
-                  })}
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      </>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Brand>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png"
+              width="40"
+              height="40"
+              alt="logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              {PaginasApp.map((item) => {
+                return (
+                  <Nav.Link as={Link} to={item.path}>
+                    {item.title}
+                  </Nav.Link>
+                );
+              })}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
   }
 }
+
 export default Menu;
